8398d5557b0e8e2bd950ab166da3129315dae63c
AXEL-11 <floressaxel@gmail.com>
2024-12-11 20:40:06 +0800

    Initial3

--- app/src/main/java/com/example/appdevfinalproject/EditProfile.java
+++ app/src/main/java/com/example/appdevfinalproject/EditProfile.java
@@ -100,7 +100,7 @@ public class EditProfile extends AppCompatActivity {
             startActivity(intent);
 
         } else {
-            Toast.makeText(this, "Failed to update profile", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Failed to update profile ", Toast.LENGTH_SHORT).show();
         }
     }
 }
